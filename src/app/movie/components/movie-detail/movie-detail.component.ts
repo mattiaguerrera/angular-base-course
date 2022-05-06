e63d2b0957308463cef8484e2f2731af7c85f848
mattiaguerrera <mattiaguerrera@outlook.com>
2022-05-06 17:50:05 +0200

    edit detail

--- src/app/movie/components/movie-detail/movie-detail.component.ts
+++ src/app/movie/components/movie-detail/movie-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { dummyMovies } from '../../models/dummy-movie';
 import { Movie } from '../../models/movie';
@@ -6,22 +8,86 @@ import { Movie } from '../../models/movie';
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
-  styleUrls: ['./movie-detail.component.css']
+  styleUrls: ['./movie-detail.component.css'],
+  providers: [DatePipe]
 })
 export class MovieDetailComponent implements OnInit {
 
   pageTitle:string = 'Movie Detail';
   movie: Movie | undefined;
   movieList: Movie[] = dummyMovies;
+  newMovie: boolean = false;
+  movieDate: string | undefined;
 
+  @ViewChild("movieForm") private movieForm: NgForm | undefined;
+  
   constructor(private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private datePipe: DatePipe ) { }
 
   ngOnInit(): void {
     const id = +this.route.snapshot.params['id'];
     if(id !== undefined) {
-      this.movie = dummyMovies.filter(m => m.id === id)[0];
+      this.movie = dummyMovies.filter(m => m.id === id)[0];      
+      this.movieDate = this.datePipe.transform(this.movie.date, 'dd/MM/yyyy')!;
+
     }
+  }
+
+  onSubmit() {
+    console.log(this.movieForm);
+  }
+
+  
+  // saveMovie() {
+  //   if (this.movieForm.valid) {
+  //     if (this.movieForm.dirty) {
+  //       const m = { ...this.movie, ...this.movieForm.value }; //override movie fields with values of form edited
+        
+  //       if (m.id === 0) {
+  //         this.movieService.createMovie(m)
+  //           .subscribe({
+  //             next: x => {
+  //               console.log(x);
+  //               return this.onSaveComplete();
+  //             },
+  //             error: err => this.errorMessage = err
+  //           });
+  //       } else {
+  //         this.movieService.updateMovie(m)
+  //           .subscribe({
+  //             next: () => this.onSaveComplete(),
+  //             error: err => this.notifyService.showWarning('Error Updating Movie', err)
+  //           });
+  //       }
+
+  //     }
+  //   } else {      
+  //     this.notifyService.showWarning('', 'Please correct the validation errors');
+  //   }
+  // }
+
+  deleteMovie(): void {
+    if (this.movie?.id === 0) {
+      // Don't delete, it was never saved.
+      this.onSaveComplete();     
+    } else if (this.movie!.id) {
+      if (confirm(`Really delete the movie: ${this.movie!.title}?`)) {
+          setTimeout(() => alert('Movie deleted!') , 1000);
+          setTimeout(() => this.router.navigate(['/movie-catalog']) , 1000);
+      }
+    }
+  }
+
+  resetForm() {
+    this.movieForm!.reset();
+    alert('Form Reset');
+  }
+
+  onSaveComplete(): void {    
+    setTimeout(() => alert('Movie saved!') , 1000);    
+    this.movieForm!.reset();
+    this.router.navigate(['/movie-catalog']);
   }
 
 }
