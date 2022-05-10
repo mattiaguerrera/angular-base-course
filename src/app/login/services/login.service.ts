df2943a8bbaac7c27c583752e90e3ed6029b9763
mattiaguerrera <mattiaguerrera@outlook.com>
2022-05-10 10:11:59 +0200

    tolto event emitter per observable

--- src/app/login/services/login.service.ts
+++ src/app/login/services/login.service.ts
@@ -1,43 +1,25 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  private eventLogin: EventEmitter<boolean> = new EventEmitter();
-
   userName: string | undefined;
   isLogged: boolean = false;
 
-  isLogged$: Observable<boolean> = new Observable(t => {
-    this.eventLogin.subscribe(event => {
-      console.log(event);
-      t.next(event);
-    });
-  });
 
   constructor(private router: Router) { }
 
   doLogon(userName: string) {
     this.userName = userName;
     this.isLogged = true;
-    //alert('SecuritySVC tells me: ' + userName);
-    this.eventLogin.emit(true);
   }
 
   logout() {
     this.isLogged = false;
     this.userName = undefined;
-    this.eventLogin.emit(false);
     this.router.navigate(['login']);
   }
 
-  // checkUser() {
-  //   if (this.isLogged == false) {
-  //     this.router.navigate(['login']);
-  //   }
-  // }
 }
